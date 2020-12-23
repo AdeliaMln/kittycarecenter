5883d74d78b42b7891b4dd9835e52b5b4f8ef1b8
utsmannn <utsmannn@gmail.com>
2020-12-23 23:08:48 +0700

    apus function artikel

--- app/Controllers/KittyCareCenter.php
+++ app/Controllers/KittyCareCenter.php
@@ -49,26 +49,6 @@ class KittyCareCenter extends BaseController
         return view('Pembayaran');
     }
 
-    public function artikel1()
-    {
-        return view('artikel1');
-    }
-
-    public function artikel2()
-    {
-        return view('artikel2');
-    }
-
-    public function artikel3()
-    {
-        return view('artikel3');
-    }
-
-    public function artikel4()
-    {
-        return view('artikel4');
-    }
-
     //--------------------------------------------------------------------
 
 }
