0d650a9ff62c096b61a7fa7853a01d854bf1717e
utsmannn <utsmannn@gmail.com>
2020-12-23 23:07:42 +0700

    nambahin controllers khusus buat artikel

---
+++ app/Controllers/Artikel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+class Artikel extends BaseController
+{
+    public function artikel1()
+    {
+        return view('artikel1');
+    }
+
+    public function artikel2()
+    {
+        return view('artikel2');
+    }
+
+    public function artikel3()
+    {
+        return view('artikel3');
+    }
+
+    public function artikel4()
+    {
+        return view('artikel4');
+    }
+
+    //--------------------------------------------------------------------
+
+}
